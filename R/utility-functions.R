b66468f7b912befa4a7fa38bcaa5cb0e848709a4
Felix Cuneo <felix.cuneo@unil.ch>
2016-04-11 14:16:29 +0200

    trymodif

--- R/utility-functions.R
+++ R/utility-functions.R
@@ -18,7 +18,8 @@ SameSense = function(VE) {
 }
 #########################
 
-
+uselessf=function(asdf){asdf+2}
+##and also some comments
 
 ToNorm = function(oneline) {
     oneline/sqrt(sum(oneline^2))
